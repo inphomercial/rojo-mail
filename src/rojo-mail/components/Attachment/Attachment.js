5748f74a36e6bbff2ce6797193855e465abddba6
Sean Brown <inphomercial@gmail.com>
2022-06-22 17:30:19 -0600

    Updating InboxItem and Attachment to use useRef to keep persistant names while hovering over. Got third inbox item column working, updated DUMMY_EMAILS to include a sub field for the greyed text

--- src/rojo-mail/components/Attachment/Attachment.js
+++ src/rojo-mail/components/Attachment/Attachment.js
@@ -1,21 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import MaterialIcon, { colorPalette } from 'material-icons-react'
 
 import './Attachment.css'
 
-const FILE_NAMES = [
-    'Invoice-E653416',
-    'Receipt-3210-0',
-    'Performance',
-    'IMG-34493',
-    'Slides PPT',
-]
+import { DUMMY_FILE_NAMES } from '../../utils/consts'
 
 export function Attachment(props) {
     const { source } = props
 
     // Gotta fix the &amp; crap.
     const url = source.url.replace(/&amp;/g, '&')
+
+    const fileName = useRef(
+        DUMMY_FILE_NAMES[Math.floor(Math.random() * DUMMY_FILE_NAMES.length)]
+    )
 
     return (
         <div className="Attachment__pill">
@@ -25,9 +23,7 @@ export function Attachment(props) {
                     color={colorPalette.red._600}
                 />
             </span>
-            <p className="Attachment__fileName">
-                {FILE_NAMES[Math.floor(Math.random() * FILE_NAMES.length)]}
-            </p>
+            <p className="Attachment__fileName">{fileName.current}</p>
 
             <div className="Attachment__image">
                 <span className="Attachment__preview">
--- src/rojo-mail/utils/consts.js
+++ src/rojo-mail/utils/consts.js
@@ -2,46 +2,83 @@ export const DUMMY_EMAILS = [
     {
         from: 'Supbase beta',
         body: 'Supabase Beta Update - May 2022',
+        sub:
+            ' - New Dark + Flutter Docs - In depth reference docs for CRUD, Auth, Realtime and more!',
     },
     {
         from: 'Apple',
         body: 'Your Apple ID information has been updated.',
+        sub:
+            ' - to your Apple ID, were made on June 18th 2022 at 2:34 PM MDT: billing information if you did not',
     },
     {
         from: 'HBO Max',
         body: 'Your Weekly HBO Max Streaming Guide',
+        sub: ' - Black voices, brides-to-be, and Borat. HBO Max',
     },
     {
         from: 'Robinhood Snacks',
-        body:
-            '#flated problems as the Fed tries to tame inflation, stocks are in the red',
+        body: '#flated problems',
+        sub: ' - ...as the Fed tries to tame inflation, stocks are in the red',
     },
     {
         from: 'Apple Arcade',
         body: 'You got 3 months of Apple Arcade free!',
+        sub:
+            ' - Redeem your offer and play 200+ incredibly fun, ad-free games, like Angry Birds Reloaded',
     },
     {
         from: 'Substack',
         body: 'A guide to going paid with your podcast on Substack',
+        sub:
+            ' - on substack.com/p/what-to-read-fictionists this week, we interviewed four writers are Substack who collectively',
     },
     {
         from: 'Zillow',
         body: '10 Results in your country - four-plex',
+        sub:
+            ' - mail.zillow.com/f/a/45JAggaSgWhhjytQW4tgWRtr~~/AAAAQA~/RFGgjajwFRaGHAsdfASDfhgRRRghhhrBAsweraWhynAE',
     },
     {
         from: 'amazon.com',
         body: 'amazon.com action needed: Sign-in',
+        sub: '',
     },
     {
         from: 'Allen at Rework',
         body:
             'Tomorrow: How checkouts internal tooling strategy scaled with the companys growth',
+        sub: '',
     },
     {
         from: 'Google Alerts',
         body:
             'Google Alert - NEWS 4 juveniles have been held at Vermonts adult jails since Wednesday',
+        sub:
+            ' - Unsubscribe from this Google Alert: Create another Google Alert: Sign in to manage your alerts',
     },
+    {
+        from: 'GitHub',
+        body: '[GitHub] Your Dependabot alerts for the week of May 31 - Jun 7',
+        sub: ' - alerts on GitHub Dependabot alerts this week',
+    },
+    {
+        from: 'Stanford University',
+        body: 'Health and medicine courses from Standford Online',
+        sub:
+            '- Expand your knowledge and advance your career with the latest courses and programs in health and medicine available now.',
+    },
+]
+
+export const DUMMY_FILE_NAMES = [
+    'Invoice-E653416',
+    'Receipt-3210-0',
+    'Performance',
+    'IMG-34493',
+    'Slides PPT',
+    '7634234123.pdf',
+    'contract-st44415',
+    'refund-5431-71',
 ]
 // 'Save up to $5,000 on a home loan during National Homeownership month',
 // "Hello Idaho! We're live in the Gem State",
